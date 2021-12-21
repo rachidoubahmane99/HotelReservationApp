c2555ed408c432ae5006943b64f184c74c7fb89f
Jadraoui Mohammed <mohamed.jadraoui7@gmail.com>
2021-12-21 20:03:12 +0100

    merge on main branch

--- HotelReservationAPP/src/java/controller/adminChambreController.java
+++ HotelReservationAPP/src/java/controller/adminChambreController.java
@@ -7,15 +7,15 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import Dao.CrudChambreDaoImp;
-import jakarta.servlet.annotation.WebServlet;
+import javax.servlet.annotation.WebServlet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import jakarta.servlet.http.HttpSession;
+import javax.servlet.http.HttpSession;
 import model.ChambreBean;
 /**
  *
